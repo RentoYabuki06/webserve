e22a7a3211f32049c0f5c4f5cb24e2c162442caa
rento yabuki <rento.nky.1203@gmail.com>
2025-09-12 19:21:09 +0900

    add:Log class

--- includes/Log.hpp
+++ includes/Log.hpp
@@ -1,18 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Log.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/19 04:57:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/09/12 19:20:37 by yabukirento      ###   ########.fr       */
+/*   Created: 2025/09/12 19:15:16 by yabukirento       #+#    #+#             */
+/*   Updated: 2025/09/12 19:19:30 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./includes/Log.hpp"
+#ifndef LOG_HPP
+#define LOG_HPP
 
-int	main(int argc, char *argv) {
-	Log::info("Server Starting ...");
-	return 0;
-}
+#include <iostream>
+#include <string>
+
+class Log
+{
+public:
+	static void info(const std::string &m) { std::cout << "[INFO] :" << m << std::endl; }
+	static void warn(const std::string &m) { std::cout << "[WARN] :" << m << std::endl; }
+	static void error(const std::string &m) { std::cout << "[ERROR] :" << m << std::endl; }
+};
+
+#endif
