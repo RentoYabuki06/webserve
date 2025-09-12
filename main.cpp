d104b3ea775df9d2c19e5ddc9d70259c235a6690
Rento Yabuki <51410980+RentoYabuki06@users.noreply.github.com>
2025-09-12 19:51:21 +0900

    Fix/makeerror (#61)
    * probe
    
    * make error fixed
    
    * fixed:makeerror

--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 04:57:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/09/12 19:20:37 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/09/12 19:28:39 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/Log.hpp"
 
-int	main(int argc, char *argv) {
+int	main(int argc, char **argv) {
+	(void) argc;
+	(void) argv;
 	Log::info("Server Starting ...");
+	Log::info("Server Finishing ...");
 	return 0;
 }
