e4d524fb08f7cb66d1e9812ec6173714e07634df
rento yabuki <rento.nky.1203@gmail.com>
2025-09-12 19:28:55 +0900

    fixed:makeerror

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: yabukirento <yabukirento@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 04:57:55 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/09/12 19:26:29 by yabukirento      ###   ########.fr       */
+/*   Updated: 2025/09/12 19:28:39 by yabukirento      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ int	main(int argc, char **argv) {
 	(void) argc;
 	(void) argv;
 	Log::info("Server Starting ...");
+	Log::info("Server Finishing ...");
 	return 0;
 }
